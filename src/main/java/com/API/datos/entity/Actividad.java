c974df5c286a2e9b331ba1f87465a86658125e85
David Alberto <eukadaju@gmail.com>
2022-06-13 22:10:45 -0500

    Actividad con mapper

--- src/main/java/com/API/datos/entity/Actividad.java
+++ src/main/java/com/API/datos/entity/Actividad.java
@@ -1,13 +1,70 @@
 package com.API.datos.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Actividad {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String titulo;
+
+    private String fechaInicio;
+
+    private String duracion;
+
+    private String estado;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "semillero_id")
+    private Semillero semillero;
+
+    public Semillero getSemillero() {
+        return semillero;
+    }
+
+    public void setSemillero(Semillero semillero) {
+        this.semillero = semillero;
+    }
 
     public Long getId() {
         return id;
--- src/main/java/com/API/security/entity/Rol.java
+++ src/main/java/com/API/security/entity/Rol.java
@@ -1,6 +1,11 @@
 package com.API.security.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 import com.API.security.enums.RolNombre;
