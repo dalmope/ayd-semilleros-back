9a4443ebef141d558d46386ccebd7addea0798bb
isaacher <johanisaacha@ufps.edu.co>
2022-06-21 08:43:56 -0500

    Empece a trabajar con archivos, hay un error en la relacion de semillero con Usuario <3

--- src/main/java/com/API/datos/controller/ProyectoController.java
+++ src/main/java/com/API/datos/controller/ProyectoController.java
@@ -2,8 +2,16 @@ package com.API.datos.controller;
 
 import com.API.datos.service.ProyectoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.mail.Multipart;
+import java.util.List;
 
 @RestController
 @RequestMapping("/proyectos")
@@ -12,5 +20,18 @@ public class ProyectoController {
     @Autowired
     ProyectoService proyectoService;
 
+
+    //Cargar Archivos
+    @PostMapping("/upload")
+    public ResponseEntity<String> cargarArchivos(@RequestParam("files") List<MultipartFile> files){
+        try {
+            proyectoService.guardarArchivo(files);
+            return ResponseEntity.status(HttpStatus.OK).body("Los archivos fueron cargados " +
+                    "correctamente al servidor");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrio " +
+                    "un error al subir el archivo");
+        }
+    }
 
 }
--- src/main/java/com/API/datos/service/ProyectoService.java
+++ src/main/java/com/API/datos/service/ProyectoService.java
@@ -3,7 +3,15 @@ package com.API.datos.service;
 import com.API.datos.entity.Proyecto;
 import com.API.datos.repository.ProyectoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class ProyectoService {
@@ -11,8 +19,31 @@ public class ProyectoService {
     @Autowired
     ProyectoRepository proyectoRepository;
 
+    //variable donde pongo la carpeta en la que se guardaran los archivos
+    public final Path rootFolder = Paths.get("uploads");
+
     public void guardarProyecto(Proyecto proyecto){
         proyectoRepository.save(proyecto);
+    }
+
+    //manejo de archivos
+
+    public void guardarArchivo(MultipartFile file) throws Exception{
+        Files.copy(file.getInputStream(), this.rootFolder.resolve(file.getOriginalFilename()));
+    }
+
+    public Resource cargarArchivo(String name) throws Exception{
+        return null;
+    }
+
+    public void guardarArchivo(List<MultipartFile> files) throws Exception{
+        for (MultipartFile file:  files){
+            this.guardarArchivo(file);
+        }
+    }
+
+    public Stream<Path> cargarArchivos() throws Exception{
+        return null;
     }
 
 }
